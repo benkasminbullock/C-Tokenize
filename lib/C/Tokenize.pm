ecfeeafbd6e8a38498263eba69f538d2a0fc9868
Ben Bullock <benkasminbullock@gmail.com>
2012-05-03 11:27:28 +0900

    Added gitignore, build.pl, README.

--- lib/C/Tokenize.pm
+++ lib/C/Tokenize.pm
@@ -7,7 +7,21 @@ C::Tokenize - reduce a C file to a series of tokens
 package C::Tokenize;
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT_OK = qw/tokenize @fields/;
+@EXPORT_OK = qw/tokenize
+                decomment
+                @fields
+                $trad_comment_re
+                $cxx_comment_re
+                $comment_re
+                $cpp_re
+                $char_const_re
+                $operator_re
+                $number_re
+                $word_re
+                $grammar_re
+                $single_string_re
+                $string_re
+               /;
 use warnings;
 use strict;
 our $VERSION = 0.01;
@@ -23,6 +37,224 @@ my $reserved_re = qr/\b(?:$reserved_words)\b/;
 
 our @fields = qw/comment cpp char_const operator grammar 
                  number word string/;
+
+=head1 REGULAR EXPRESSIONS
+
+The regular expressions can be imported using, for example,
+
+    use C::Tokenize '$cpp_re'
+
+to import C<$cpp_re>.
+
+None of the regular expressions does any capturing. If you want to
+capture, add your own parentheses around the regular expression.
+
+=over
+
+=item $trad_comment_re
+
+Match C</* */> comments.
+
+=item $cxx_comment_re
+
+Match C<//> comments.
+
+=item $comment_re
+
+Match both C</* */> and C<//> comments.
+
+=item $cpp_re
+
+Match a C preprocessor instruction.
+
+=item $char_const_re
+
+Match a character constant, such as C<'a'> or C<'\-'>.
+
+=item $operator_re
+
+Match an operator such as C<+> or C<-->.
+
+=item $number_re
+
+Match a number, either integer, floating point, or hexadecimal. Does
+not do octal yet.
+
+=item $word_re
+
+Match a word, such as a function or variable name or a keyword of the
+language.
+
+=item $grammar_re
+
+Match other syntactic characters such as C<{> or C<[>.
+
+=item $single_string_re
+
+Match a single C string constant such as C<"this">.
+
+=item $string_re
+
+Match a full-blown C string constant, including compound strings
+C<"like" "this">.
+
+=cut
+
+    # Regular expression to match a /* */ C comment.
+
+our $trad_comment_re = qr!
+                            /\*
+                            (?:
+                                # Match "not an asterisk"
+                                [^*]
+                            |
+                                # Match multiple asterisks followed
+                                # by anything except an asterisk or a
+                                # slash.
+                                \*+[^*/]
+                            )*
+                            # Match multiple asterisks followed by a
+                            # slash.
+                            \*+/
+                        !x;
+
+# Regular expression to match a // C comment (C++-style comment).
+
+our $cxx_comment_re = qr!//.*\n!;
+
+# Master comment regex
+
+our $comment_re = qr/
+                       (?:
+                           $trad_comment_re
+                       |
+                           $cxx_comment_re
+                       )
+                   /x;
+
+# Regular expression to match a C preprocessor instruction
+
+our $cpp_re = qr/^\h*\#(?:
+                    $trad_comment_re
+                |
+                    [^\\\n]
+                |
+                    \\[^\n]
+                |
+                    \\\n
+                )+\n
+               /mx;
+
+# Regular expression to match a C character constant like 'a' or '\0'.
+# This allows any \. expression at all.
+
+our $char_const_re = qr/
+                          '
+                          (?:
+                              .
+                          |
+                              \\.
+                          )
+                          '
+                      /x;
+
+# Regular expression to match one character operators
+
+our $one_char_op_re = qr/(?:\%|\&|\+|\-|\=|\/|\||\.|\*|\:|>|<|\!|\?|~|\^)/;
+
+# Regular expression to match all operators
+
+our $operator_re = qr/
+                        (?:
+                                # Operators with two characters
+                                \|\||&&|<<|>>|--|\+\+|->
+                            |
+                                # Operators with one or two characters
+                                # followed by an equals sign.
+                                (?:<<|>>|\+|-|\*|\/|%|&|\||\^)
+                                =
+                            |
+                                $one_char_op_re
+                            )
+                    /x;
+
+# Re to match a C number
+
+our $decimal_re = qr/[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?l?/i;
+
+our $hex_re = qr/0x[0-9a-f]+l?/i;
+
+our $number_re = qr/
+                      (?:
+                          $hex_re
+                      |
+                          $decimal_re
+                      )
+                  /x;
+
+# Re to match a C word
+
+our $word_re = qr/[a-z_](?:[a-z_0-9]*)/i;
+
+# Re to match C grammar
+
+our $grammar_re = qr/[(){};,\[\]]/;
+
+# Regular expression to match a C string.
+
+our $single_string_re = qr/
+                             (?:
+                                 "
+                                 (?:[^\\"]+|\\[^"]|\\")*
+                                     "
+                                     )
+                         /x;
+
+
+# Compound string regular expression.
+
+our $string_re = qr/$single_string_re(?:\s*$single_string_re)*/;
+
+# Master regular expression for tokenizing C text. This uses named
+# captures.
+    
+our $c_re = qr/
+                 (?<leading>\s+)?
+                 (?:
+                     (?<comment>$comment_re)
+                 |
+                     (?<cpp>$cpp_re)
+                 |
+                     (?<char_const>$char_const_re)
+                 |
+                     (?<operator>$operator_re)
+                 |
+                     (?<grammar>$grammar_re)
+                 |
+                     (?<number>$number_re)
+                 |
+                     (?<word>$word_re)
+                 |
+                     (?<string>$string_re)
+                 )
+             /x;
+
+
+=head2 decomment
+
+    my $out = decomment ('/* comment */');
+    # $out = " comment ";
+
+Remove the comments from a string.
+
+=cut
+
+sub decomment
+{
+    my ($comment) = @_;
+    $comment =~ s/^\/\*(.*)\*\/$/$1/sm;
+    return $comment;
+}
 
 =head2 tokenize
 
@@ -59,139 +291,8 @@ sub tokenize
 
     my @tokens;
 
-    # Regular expression to match a /* */ C comment.
-
-    my $trad_comment_re = qr!
-                             /\*
-                             (?:
-                                 [^\*]
-                             |
-                                 \*+[^/]
-                             )*
-                             \*/
-                         !x;
-
-    # Regular expression to match a // C comment (C++-style comment).
-
-    my $cxx_comment_re = qr!//.*\n!;
-
-    # Master comment regex
-
-    my $comment_re = qr/
-                           (?:
-                               $trad_comment_re
-                           |
-                               $cxx_comment_re
-                           )
-                       /x;
-
-    # Regular expression to match a C preprocessor instruction
-
-    my $cpp_re = qr/^\h*\#(?:
-                        [^\\\n]+
-                    |
-                        \\[^\n]
-                    |
-                        \\\n
-                    )+\n
-                   /mx;
-
-    # Regular expression to match a C character constant like 'a' or '\0'.
-    # This allows any \. expression at all.
-
-    my $char_const_re = qr/
-                              '
-                              (?:
-                                  .
-                              |
-                                  \\.
-                              )
-                              '
-                          /x;
-
-    # Regular expression to match one character operators
-
-    my $one_char_op = qr/(?:\%|\&|\+|\-|\=|\/|\||\.|\*|\:|>|<|\!|\?)/;
-
-    # Regular expression to match all operators
-
-    my $operator_re = qr/
-                            (?:
-                                \|\||&&|<<|>>
-                            |
-                                $one_char_op
-                                =
-                            |
-                                $one_char_op
-                            )
-                        /x;
-
-    # Re to match a C number
-
-    my $decimal_re = qr/[0-9.e]+l?/i;
-
-    my $hex_re = qr/0x[0-9a-f]+/i;
-
-    my $number_re = qr/
-                          (?:
-                              $decimal_re
-                          |
-                              $hex_re
-                          )
-                      /x;
-
-    # Re to match a C word
-
-    my $word_re = qr/[a-z_](?:[a-z_0-9]*)/i;
-
-    # Re to match C grammar
-
-    my $grammar_re = qr/[(){};,\[\]]/;
-
-    # Regular expression to match a C string.
-
-    my $single_string_re = qr/
-                          (?:
-                              "
-                              (?:[^\\"]+|\\[^"]|\\")*
-                              "
-                           )
-                           /x;
-
-
-    # Compound string regular expression.
-
-    my $string_re = qr/$single_string_re(?:\s*$single_string_re)*/;
-
-    # Master regular expression for tokenizing C text.
-    
-    my $c_re = qr/
-                  (?<leading>\s+)?
-                  (?:
-                      (?<comment>$comment_re)
-                  |
-                      (?<cpp>$cpp_re)
-                  |
-                      (?<char_const>$char_const_re)
-                  |
-                      (?<operator>$operator_re)
-                  |
-                      (?<grammar>$grammar_re)
-                  |
-                      (?<number>$number_re)
-                  |
-                      (?<word>$word_re)
-                  |
-                      (?<string>$string_re)
-                  )
-                 /x;
-
-
-#    print length $text, "\n";
-#    print "$c_re\n";
     my $line = 1;
     while ($text =~ /\G($c_re)/g) {
-#        print "ok\n";
         my $match = $1;
         if ($match =~ /^\s+$/s) {
             die "Bad match.\n";
@@ -271,5 +372,20 @@ sub get_line_number
     die "$pos outside bounds";
 }
 
+=head1 BUGS
+
+=over
+
+=item Octal not parsed
+
+It does not parse octal expressions.
+
+=item trigraphs
+
+No handling of trigraphs.
+
+=back
+
+=cut
 
 1;
--- t/C-Tokenize.t
+++ t/C-Tokenize.t
@@ -1,8 +1,68 @@
 use warnings;
 use strict;
-use Test::More tests => 1;
+use Test::More tests => 6;
 BEGIN { use_ok('C::Tokenize') };
-use C::Tokenize;
+BEGIN { use_ok('C::Tokenize', '$trad_comment_re', 'decomment') };
+use C::Tokenize 'tokenize';
+
+my $tokens;
+
+# Test for not eating subsequent text up to another comment.
+
+my $long_comment =<<'EOF';
+/****************************************************************************
+
+  Globals read from font file
+
+****************************************************************************/
+
+char		hardblank;
+int		charheight;
+/* Bogus */
+EOF
+
+$tokens = tokenize ($long_comment);
+
+my $found;
+
+if (0) {
+printf "%d\n", scalar @$tokens;
+for my $token (@$tokens) {
+    print $token->{type}, " -> ", $token->{$token->{type}}, "\n";
+}
+}
+for my $token (@$tokens) {
+    my $type = $token->{type};
+    my $value = $token->{$type};
+    if ($type ne 'comment' && $value =~ /charheight/) {
+        $found = 1;
+    }
+}
+ok ($found, "Parsing of long comments with multiple asterisks.");
+
+# Test for comments within preprocessor instructions
+
+my $cpp_comment =<<'EOF';
+#define SMO_YES 1		/* use command-line smushmode, ignore font
+				 * smushmode */
+EOF
+$tokens = tokenize ($cpp_comment);
+# This prints out details in case this bug appears again.
+if (0) {
+printf "%d\n", scalar @$tokens;
+for my $token (@$tokens) {
+    print $token->{type}, " ", $token->{$token->{type}}, "\n";
+}
+}
+ok (@$tokens == 1, "Comment in CPP with newline");
+
+ok ($long_comment =~ /$trad_comment_re/);
+
+my $stuff = "babu\nchabu";
+my $comment = "/*$stuff*/";
+my $decommented = decomment ($comment);
+
+ok ($decommented eq $stuff, "Test decomment for multiline comments");
 
 # Local variables:
 # mode: perl
