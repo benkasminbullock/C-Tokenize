180f3886b9c7464616f23a2707582e28265e188a
Ben Bullock <benkasminbullock@gmail.com>
2016-12-07 10:29:33 +0900

    Better documentation including repo links

--- make-pod.pl
+++ make-pod.pl
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 use Template;
 use FindBin '$Bin';
-use Perl::Build qw/get_version get_commit/;
+use Perl::Build qw/get_commit get_info/;
 use Perl::Build::Pod ':all';
 use Deploy qw/do_system older/;
 use Getopt::Long;
@@ -19,8 +19,8 @@ my %pbv = (
     base => $Bin,
     verbose => $verbose,
 );
-my $version = get_version (%pbv);
 my $commit = get_commit (%pbv);
+my $info = get_info (%pbv);
 # Names of the input and output files containing the documentation.
 
 my $pod = 'Tokenize.pod';
@@ -30,8 +30,8 @@ my $output = "$Bin/lib/C/$pod";
 # Template toolkit variable holder
 
 my %vars = (
-    version => $version,
     commit => $commit,
+    info => $info,
 );
 
 my $tt = Template->new (
